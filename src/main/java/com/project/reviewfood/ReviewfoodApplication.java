3434f29cb292c3adf800055b55bebe129fef66e4
LePhuThanh <lpthanh710@gmail.com>
2023-09-27 15:35:55 +0700

    init project & connect data to MySQL

---
+++ src/main/java/com/project/reviewfood/ReviewfoodApplication.java
@@ -0,0 +1,13 @@
+package com.project.reviewfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/reviewfood/ReviewfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reviewfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
